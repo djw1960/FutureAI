281f79773ce2abf4d0d6dde1a2e4471b9cdce91f
djw1960git <djw1960@126.com>
2018-06-26 00:45:00 +0800

    静态页面

---
+++ FutureProj/Future.UI/FrozenVer/js/common.js
@@ -0,0 +1,141 @@
+﻿; (function ($) {
+    //获取时间戳
+    $.getLocalTime = function (at, fmt) {
+        if (!at) return '-';
+        if (typeof at == 'string') {
+            at = new Date(at.replace(/-/g, '/'));
+        } else {
+            at = new Date(at);
+        }
+        var o = {
+            "M+": at.getMonth() + 1, //月份 
+            "d+": at.getDate(), //日 
+            "h+": at.getHours(), //小时 
+            "m+": at.getMinutes(), //分 
+            "s+": at.getSeconds(), //秒 
+            "q+": Math.floor((at.getMonth() + 3) / 3), //季度 
+            "S": at.getMilliseconds() //毫秒 
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (at.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    };
+    //字符串操作
+    $.str_length = function (str) {
+        var realLength = 0, len = str.length, charCode = -1;
+        for (var i = 0; i < len; i++) {
+            charCode = str.charCodeAt(i);
+            if (charCode >= 0 && charCode <= 128) realLength += 1;
+            else realLength += 2;
+        }
+        return realLength;
+    }
+    $.str_cut = function (str, len, left) {
+        var newLength = 0;
+        var newStr = "";
+        var chineseRegex = /[^\x00-\xff]/g;
+        var singleChar = "";
+        var strLength = str.replace(chineseRegex, "**").length;
+        left = left || '...';
+        for (var i = 0; i < strLength; i++) {
+            singleChar = str.charAt(i).toString();
+            if (singleChar.match(chineseRegex) != null) {
+                newLength += 2;
+            }
+            else {
+                newLength++;
+            }
+            if (newLength > len) {
+                break;
+            }
+            newStr += singleChar;
+        }
+
+        if (strLength > len) {
+            newStr += left;
+        }
+        return newStr;
+    }
+    $.getParams = function (type) {
+        var result = {};
+        var params = (window.location.search.split('?')[1] || '').split('&');
+        for (var param in params) {
+            if (params.hasOwnProperty(param)) {
+                paramParts = params[param].split('=');
+                result[paramParts[0]] = decodeURIComponent(paramParts[1] || "");
+            }
+        }
+        if (type) {
+            return result[type];
+        }
+        return result;
+    };
+    //简化ajax方法
+    $.ajaxF = function (options) {
+        var defaults = {
+            type: 'post',
+            url: options.url,
+            dataType: 'json',
+            timeout: 8000,
+            beforeSend: function () {
+                return true;
+            },
+            complete: function () {
+                return true;
+            },
+            success: function () {
+                return true;
+            },
+            error: function (data) {
+                $.toast("网络出错，请重试！");
+            },
+            fail: function () {
+                //返回错误回调函数
+                return true;
+            }
+        };
+        var callback = {};
+        if (typeof options.success == 'function') {
+            callback.success = function (data) {
+                if (data.ok != 1 || data.code != 0) {
+                    if (typeof options.fail == 'function') {
+                        options.fail(data);
+                    } else {
+                        $.toast(data.msg);
+                    }
+                    return true;
+                }
+                return options.success(data);
+            }
+        };
+        //配置loading
+        if (options.loading) {
+            var overlay = $.overlay(options.loading);
+            callback.beforeSend = function () {
+                overlay.show();
+            };
+            callback.complete = function () {
+                overlay.remove();
+            }
+        }
+        return $.ajax($.extend({}, defaults, options, callback));
+    };
+    $.overlay = function (dom) {
+        if (typeof dom === 'string') {
+            $dom = $(dom);
+            $dom.css({ position: 'relative' })
+        };
+        $dom.append('<div class="ui-loading-block overlay"><div class="ui-loading-cnt overlayload" ><i class="ui-loading-bright"></i><p>正在加载中...</p></div></div>');
+        overlay = $dom.find(".overlay");
+        if (dom == 'body') {
+            $(".overlay").css({ "position": "fixed" });
+            $dom.css({ position: '' })
+        };
+        return overlay;
+    };
+    $.toast = function (msg) {
+        var msgm = '<div class="ui-tooltips ui-tooltips-warn ui-tooltips-hignlight"><div class="ui-tooltips-cnt ui-tooltips-cnt-link ui-border-b"><i></i>' + msg+'</div ></div >';
+        $('body').append(msgm);
+    }
+})(window.Zepto);
--- FutureProj/Future.UI/FrozenVer/js/main.js
+++ FutureProj/Future.UI/FrozenVer/js/main.js
@@ -1,47 +1,99 @@
 ﻿(function (doc, win) {
-    $('#list td,#list li,.linklist li').click(function () {
-        location.href = $(this).data('href');
-    });
+    var future = {
+        data: {
+            urls: "http://www.doapi.info/api/futurecenter",
+            type: 'dce',
+            cate: $.getParams("cate")||'n',
+            code: $.getParams("code") || '',
+            page: 0,
+        },
+        init: function () {
+            var self = this;
+            self.bindevent();
+        },
+        ajax: null,
+        bindevent: function () {
+            var self = this;
+            $('#list td,#list li,.linklist li').click(function () {
+                location.href = $(this).data('href');
+            });
+            //tab切换
+            $('#ntabcontent>.ui-tab-nav').eq(0).find('li').on('click', function () {
+                $(this).parent().find('li').removeClass('current');
+                $(this).addClass('current');
+                //加载tab数据-切换tabContent
+                self.loadnews(this);            
+            });
 
-    $('.ui-header .ui-btn').click(function () {
-        location.href = 'index.html';
-    });
+            //tab切换
+            $('#cdtabcontent>.ui-tab-nav').eq(0).find('li').on('click', function () {
+                $(this).parent().find('li').removeClass('current');
+                $(this).addClass('current');
+                //加载tab数据-切换tabContent
+                self.loadcd(this);
+            });
+            $("#ntabcontent>.ui-tab-content ul>li").live("click", function () {
+                location.href = $(this).data('href');
+            })
+        },
+        turncontent: function (conthtml) {
 
-    $("#btn1").click(function () {
-        $('.ui-actionsheet').addClass('show');
-    });
+        },
+        loadnews: function (ethisobj) {
+            var self = this;
+            var params = {
+                no: 1010,
+                page: self.data.page,
+                type: self.data.type,
+            };
 
-    $("#cancel").click(function () {
-        $(".ui-actionsheet").removeClass("show");
-    });
-    //tab切换
-    $('#ntabcontent>.ui-tab-nav').eq(0).find('li').on('click', function () {
-        $(this).parent().find('li').removeClass('current');
-        $(this).addClass('current');
-        //加载tab数据-切换tabContent
-        var lihtml = "<ul class=\"ui-list ui-border-tb \"><li><div class=\"ui-list-img-square\"><span style=\"background-image:url(http://placeholder.qiniudn.com/188x188)\" ></span> </div><div class=\"ui-list-info ui-border-t\"><h4 class=\"ui-nowrap\">这是标题，加ui-nowrap可以超出长度截断</h4><p class=\"ui-nowrap\">这是内容，加ui-nowrap可以超出长度截断</p></div></li></ul>";
-        $('.ui-tab-content>li').eq($(this).index()).html(lihtml);
-        $('.ui-tab-content').eq(0).css({
-            'transform': 'translate3d(-' + ($(this).index() * $('.ui-tab-content li').offset().width) + 'px,0,0)',
-            'transition': 'transform 0.5s linear'
-        })
-    });
+            $.ajax({
+                type: 'POST',
+                url: self.data.urls,
+                data: params,
+                dataType: 'json',
+                timeout: 9000,
+                success: function (data) {
+                    if (data.code == 0) {
+                        var list = data.data.list;
+                        var lihtml = '<ul class=\"ui-list ui-border-tb \">';
+                        for (var i = 0; i < list.length; i++) {
+                            var item = list[i];
+                            lihtml += '<li data-href="./n/d.html"><div class=\"ui-list-img-square\"><span style=\"background-image:url(http://placeholder.qiniudn.com/188x188)\" ></span> </div><div class=\"ui-list-info ui-border-t\"><h4 class=\"ui-nowrap\">' + item.NewsTitle + '</h4><p class=\"ui-nowrap\">来源：' + item.NSource + '日期：' + item.PublishDate+'</p></div></li>';
+                        }
+                        lihtml += '</ul">';
+                        $('.ui-tab-content>li').eq($(ethisobj).index()).html(lihtml);
+                        $('.ui-tab-content').eq(0).css({
+                            'transform': 'translate3d(-' + ($(ethisobj).index() * $('.ui-tab-content li').offset().width) + 'px,0,0)',
+                            'transition': 'transform 0.5s linear'
+                        })
+                    }
+                },
+                error: function (xhr, type) {
+                    alert('Ajax error!')
+                },
+                beforeSend: function () {
+                    $.overlay("body").show();
+                },
+                complete: function () {
+                    $.overlay("body").hide();
+                }
+            })
+        },
+        loadcd: function (ethisObj) {
+            var self = this;
+            var lihtml = '<ul class="ui-list ui-border - tb ">';
+            lihtml += '<li class="ui-border-t" style= "margin-left:0px;" >';
+            lihtml += '<ul class="ui-row contenthr"><li class="ui-col ui-col-25 ui-border-r contentdh">名称</li><li class="ui-col ui-col-25 contentdd">价格</li>';
+            lihtml += '<li class="ui-col ui-col-25 contentdd">涨跌额</li><li class="ui-col ui-col-25 contentdd">涨跌幅</li></ul></li >';
+            lihtml += '<li class="ui-border-t" style="margin-left:0px;"><ul class="ui-row contentdr"><li class="ui-col ui-col-25 ui-border-r contentdh">螺纹钢</li><li class="ui-col ui-col-25 contentdd">4238</li><li class="ui-col ui-col-25 contentdd">+79</li> <li class="ui-col ui-col-25 contentdd">+0.6%</li></ul> </li></ul>';
 
-    //tab切换
-    $('#cdtabcontent>.ui-tab-nav').eq(0).find('li').on('click', function () {
-        $(this).parent().find('li').removeClass('current');
-        $(this).addClass('current');
-        //加载tab数据-切换tabContent
-        var lihtml = '<ul class="ui-list ui-border - tb ">';
-        lihtml += '<li class="ui-border-t" style= "margin-left:0px;" >';
-        lihtml += '<ul class="ui-row contenthr"><li class="ui-col ui-col-25 ui-border-r contentdh">名称</li><li class="ui-col ui-col-25 contentdd">价格</li>';
-        lihtml += '<li class="ui-col ui-col-25 contentdd">涨跌额</li><li class="ui-col ui-col-25 contentdd">涨跌幅</li></ul></li >';
-        lihtml += '<li class="ui-border-t" style="margin-left:0px;"><ul class="ui-row contentdr"><li class="ui-col ui-col-25 ui-border-r contentdh">螺纹钢</li><li class="ui-col ui-col-25 contentdd">4238</li><li class="ui-col ui-col-25 contentdd">+79</li> <li class="ui-col ui-col-25 contentdd">+0.6%</li></ul> </li></ul>';           
-                         
-        $('.ui-tab-content>li').eq($(this).index()).html(lihtml);
-        $('.ui-tab-content').eq(0).css({
-            'transform': 'translate3d(-' + ($(this).index() * $('.ui-tab-content li').offset().width) + 'px,0,0)',
-            'transition': 'transform 0.5s linear'
-        })
-    });
+            $('.ui-tab-content>li').eq($(ethisObj).index()).html(lihtml);
+            $('.ui-tab-content').eq(0).css({
+                'transform': 'translate3d(-' + ($(ethisObj).index() * $('.ui-tab-content li').offset().width) + 'px,0,0)',
+                'transition': 'transform 0.5s linear'
+            })
+        }
+    };
+    future.init();
 })(document, window);
